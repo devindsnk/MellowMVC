181837990a019d05595f1cb7ace25e20fb8d6f7c
daredevil25 <devindsnk25@gmail.com>
2022-04-08 10:53:18 +0530

    Adding content to readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MellowMVC
-A PHP framework based on MVC architecture
+A simple PHP framework based on MVC Architecture with a custom SQL Query Builder. 
+
+Developed for the 2nd year group project SCS2202.
+Architecture is built following the tutorial by **Brad Traversy**.
+https://www.udemy.com/course/object-oriented-php-mvc/
+
+## How to use
+- Clone the repository to your localhost folder
+git clone https://github.com/daredevil25/MellowMVC.git
+
+- Import the SQL dump file 
+public/resources/mellowmvc_db.sql
+
+- Visit the site 
+https://localhost/MellowMVC/home
---
+++ public/resources/mellowmvc_db.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 08, 2022 at 05:19 AM
+-- Server version: 8.0.27
+-- PHP Version: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mellowmvc_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdata`
+--
+
+DROP TABLE IF EXISTS `userdata`;
+CREATE TABLE IF NOT EXISTS `userdata` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fName` varchar(50) NOT NULL,
+  `lName` varchar(50) NOT NULL,
+  `age` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `userdata`
+--
+
+INSERT INTO `userdata` (`id`, `fName`, `lName`, `age`) VALUES
+(5, 'Kevin', 'McCallister', 15),
+(6, 'Raymond', 'Babbitt', 37);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
